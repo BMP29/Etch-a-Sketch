b275eb47b4a79bd813b74cad610593a80fbffce0
BMP29 <brenomp730@gmail.com>
2023-07-29 14:08:34 -0300

    New Ui implemented & minor changes in the code
    I'm feeling well in the last days, so I didn't made much commits.
    Sorry for the bad code

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const DEFAULTCOLOR = '#000000';
 let color = DEFAULTCOLOR;
 
+let isMouseDown = false;
+let isRandom = false;
+
 //draw the grid
 function drawGrid(size) {
     const grid = document.querySelector('#grid');
-
+    const paraSize = document.getElementById('sizePara');
+    paraSize.innerHTML = `${size} x ${size}`;
     for(let i = 0; i < size; i++) {
         const row = document.createElement('div');
         row.classList = 'row';
@@ -15,25 +19,34 @@ function drawGrid(size) {
             square.style.flex = '1';
             square.classList = 'squares';
             square.setAttribute('draggable', false);
+            square.style.backgroundColor = '#FFFFFF'
             row.appendChild(square);
         }
     }
+    paintSquares();
+}
+ 
+//the following code allow the squares to change color when the table HEAR a mousedown...
+//...and the square (grid-elements) HEAR that the mouse is over then
+function paintSquares() {
+    const gridElements = document.querySelectorAll('.squares');
+    gridElements.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            if(isMouseDown && !isRandom) square.style.backgroundColor = color;
+            else if(isMouseDown && isRandom) square.style.backgroundColor = randomColors();
+            console.log("MouseOver");
+        });
+    });
 }
 
-drawGrid(16);
+function randomColors() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    color = `rgb(${red}, ${green}, ${blue}`;
+    return color;
+}
 
-//the following code changes the square's color when the table is HEAR a mousedown...
-//...and the square (grid-elements) HEAR that the mouse is over then
-const grid = document.getElementById('grid');
-const gridElements = document.querySelectorAll('.squares');
-let isMouseDown = false;
-
-gridElements.forEach(square => {
-    square.addEventListener('mouseover', () => {
-        if(isMouseDown) square.style.backgroundColor = color;
-        console.log("hovering");
-    });
-});
 
 grid.addEventListener('mousedown', () => {
     isMouseDown = true;
@@ -51,10 +64,35 @@ grid.addEventListener('mouseup', () => {
 });
 
 
+drawGrid(16);
+
 //changes the color used to paint the squares
 const colorPicker = document.getElementById("color-picker");
 colorPicker.addEventListener("input", function() {
- color = this.value;
-  console.log( color);
+    color = this.value;
+    isRandom = false;
+    console.log(color);
 });
 
+function resetGrid() {
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(row => {
+        grid.removeChild(row);
+    });
+    drawGrid(+slider.value);
+}
+
+const slider = document.getElementById('myRange');
+slider.addEventListener('mouseup', resetGrid);
+
+const btnClear = document.getElementById('clear');
+btnClear.addEventListener('click', resetGrid);
+
+const btnRanColors = document.getElementById('ranColors');
+btnRanColors.addEventListener('click', () => {
+    if(isRandom) isRandom = false;
+    else {
+        isRandom = true;
+        color = colorPicker.value;
+    }
+});
